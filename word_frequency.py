8b452c5b75b7ecfa70c620caf12bdc43eea5ab73
Christina Cornell <cdhorn515@gmail.com>
2017-10-12 13:03:01 -0400

    adding words to dictionary and counting occurances newline characters still showing

--- word_frequency.py
+++ word_frequency.py
@@ -2,7 +2,16 @@ import re
 
 with open("sample.txt") as file:
     lines = file.read()
-print(type(lines))
-# returns a str
-lines = re.sub("[^a-zA-Z\s]", "", lines).lower()
-print(lines)
+
+def add_words_to_dictionary(lines):
+    words = re.sub(r"[^a-zA-Z\s]", "", lines).lower().split(" ")
+
+    word_dict = {}
+    for word in words:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+    return word_dict
+
+print(add_words_to_dictionary(lines))
