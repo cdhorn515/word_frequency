3f354850c9cdd349ce870c166554ad08cfc34073
Christina Cornell <cdhorn515@gmail.com>
2017-10-12 14:18:25 -0400

    sorting done, top 20 words and number of occurences displays

--- word_frequency.py
+++ word_frequency.py
@@ -4,8 +4,9 @@ with open("sample.txt") as file:
     lines = file.read()
 
 def add_words_to_dictionary(lines):
-    words = re.sub(r"[^a-zA-Z\s]", "", lines).lower().split(" ")
+    words = re.sub(r"[^a-zA-Z\s]", "", lines).lower().split()
 
+    global word_dict
     word_dict = {}
     for word in words:
         if word in word_dict:
@@ -14,4 +15,11 @@ def add_words_to_dictionary(lines):
             word_dict[word] = 1
     return word_dict
 
-print(add_words_to_dictionary(lines))
+add_words_to_dictionary(lines)
+
+def sort_dictionary(word_dict):
+    sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+    for word, number in sorted_list[:20]:
+        print(word + ": " + str(number))
+
+sort_dictionary(word_dict)
