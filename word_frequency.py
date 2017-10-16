8e635e643487b38088b01f6369df0aede218fe5c
Christina Cornell <cdhorn515@gmail.com>
2017-10-16 15:58:13 -0400

    added main method

--- word_frequency.py
+++ word_frequency.py
@@ -1,25 +1,32 @@
 import re
 
-with open("sample.txt") as file:
-    lines = file.read()
 
-def add_words_to_dictionary(lines):
-    words = re.sub(r"[^a-zA-Z\s]", "", lines).lower().split()
+def main():
+    with open("sample.txt") as file:
+        lines = file.read()
 
-    global word_dict
-    word_dict = {}
-    for word in words:
-        if word in word_dict:
-            word_dict[word] += 1
-        else:
-            word_dict[word] = 1
-    return word_dict
 
-add_words_to_dictionary(lines)
+    def add_words_to_dictionary(lines):
+        words = re.sub(r"[^a-zA-Z\s]", "", lines).lower().split()
 
-def sort_dictionary(word_dict):
-    sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
-    for word, number in sorted_list[:20]:
-        print(word + ": " + str(number))
+        global word_dict
+        word_dict = {}
+        for word in words:
+            if word in word_dict:
+                word_dict[word] += 1
+            else:
+                word_dict[word] = 1
+        return word_dict
 
-sort_dictionary(word_dict)
+    add_words_to_dictionary(lines)
+
+
+    def sort_dictionary(word_dict):
+        sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+        for word, number in sorted_list[:20]:
+            print(word + ": " + str(number))
+
+    sort_dictionary(word_dict)
+
+if __name__ == '__main__':
+    main()
