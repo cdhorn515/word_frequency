5fda571a1f5a44b096d29e1ce0c71bf8bc4720fe
Christina Cornell <cdhorn515@gmail.com>
2017-10-20 15:17:45 -0400

    updated code line spacing to conform to pep8 guideline

--- word_frequency.py
+++ word_frequency.py
@@ -5,10 +5,8 @@ def main():
     with open("sample.txt") as file:
         lines = file.read()
 
-
     def add_words_to_dictionary(lines):
         words = re.sub(r"[^a-zA-Z\s]", "", lines).lower().split()
-
         global word_dict
         word_dict = {}
         for word in words:
@@ -20,13 +18,13 @@ def main():
 
     add_words_to_dictionary(lines)
 
-
     def sort_dictionary(word_dict):
         sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
         for word, number in sorted_list[:20]:
             print(word + ": " + str(number))
 
     sort_dictionary(word_dict)
+
 
 if __name__ == '__main__':
     main()
